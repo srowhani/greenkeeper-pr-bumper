b1c7f3c2ce4fc9969291414221525b7c5aa13f36
Seena <seenarowhani95@gmail.com>
2016-08-24 14:03:01 -0400

    axios

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+(function (createHandler, http, axios) {
+  var handler = createHandler({ path: '/webhook', secret: process.env.SECRET })
+  http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
+    })
+  }).listen(process.env.PORT || 3000)
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
+  handler.on('error', function (err) {
+    console.error('Error:', err.message)
   })
-}).listen(process.env.PORT || 3000)
 
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
+  handler.on('pull_request_review_comment', function (event) {
 
-handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref)
-})
+    var comment = event.payload.comment
+    var url = comment.url
+    var body = comment.body
+    var user = comment.user.login
+    
+    console.log('hit a PR comment from ' + user + 'at ' + url)
 
-handler.on('issues', function (event) {
-  console.log('Received an issue event for %s action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title)
-})
-
-handler.on('pull_request_review_comment', function (event) {
-  console.log('woo')
-  console.log(event)
-})
+    if (user === 'greenkeeperio-bot') {
+      console.log('Message from greenkeeperio-bot detected')
+      axios.create({
+        url: url,
+        method: 'patch',
+        headers: {
+          'Authorization: token' + process.env.TOKEN
+        },
+        data: {
+          body: '#PATCH#' + body
+        }
+      }).then(function (response) {
+        console.log(response)
+      }).catch(function (error) {
+        console.log(error)
+      })
+    }
+  })
+})(
+  require('github-webhook-handler'),
+  require('http'),
+  require('axios')
+)
